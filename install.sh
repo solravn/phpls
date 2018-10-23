b177b5b54985ac1d6fe66de99e9543d2e3108425
grizmar <grizmar5@gmail.com>
2018-10-23 23:54:35 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -rf ./src ./xhprof
+git clone -b badoo-7.0 https://github.com/tony2001/xhprof.git ./xhprof
+git clone git@github.com:grizmar/oxana.git ./src
+docker-compose run --workdir=/var/www/oxana php composer install
