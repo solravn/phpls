cb62478d6c873cf3d6bb1a49cc312ba0b5105872
Ildar Gafurov <ildar.gafurov@pimpay.ru>
2021-11-12 14:57:03 +0300

    Fixed postgres docker config

--- etc/docker/postgres/docker-entrypoint-initdb.d/init_db_dev.sh
+++ etc/docker/postgres/docker-entrypoint-initdb.d/init_db_dev.sh
@@ -2,6 +2,5 @@
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
    CREATE USER dev password '123' superuser;
-   CREATE DATABASE postgres owner dev LC_COLLATE='ru_RU.UTF-8' LC_CTYPE='ru_RU.UTF-8' TEMPLATE template0;
+   CREATE DATABASE dev OWNER dev LC_COLLATE='en_US.utf8' LC_CTYPE='en_US.utf8' TEMPLATE template0;
 EOSQL
-export PGDATA=/var/lib/postgresql/data
